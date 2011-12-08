6b096c5110daf875720d534c55e0fadd119542e6
Franklin Webber <franklin.webber@gmail.com>
2011-12-08 06:28:23 -0800

    Suggestions:
      * Refactored to a single render method as both locations were
        using the same output.
    
      * Used a map operation and a join on the results to have it collect
        all the results.
    
      * Minor formatting changes to be more 'ruby'
    
      * Updated the validation to make empty last names "   " to be rejected.
        Though I would recommend Highline for better validation of applications.
    
      * Removed some un-needed assigment of variables for most of the methods
    
      * Showed how you can get the results of a tuple into each value
        with the firstname, lastname, location

--- lib/people_search.rb
+++ lib/people_search.rb
@@ -4,7 +4,6 @@ require 'net/http'
 require 'net/https'
 require 'uri'
 require 'sinatra'
-require 'haml'
 
 # search for a person based on first name, last name and location.  Location can be city and state or state or zip
 def person_search(fname, lname, where)
@@ -17,10 +16,8 @@ def person_search(fname, lname, where)
 
   resp = connection.request_get(uri.path + '?' + uri.query)
 
-### make sure to throw an error if we get a 200 returned
-  if resp.code != '200'
-     raise "web service error"
-  end
+  ### make sure to throw an error if we get a 200 returned
+  raise "web service error" if resp.code != '200'
 
   data = resp.body
 
@@ -29,55 +26,51 @@ def person_search(fname, lname, where)
   result = JSON.parse(data)
 
   # if the hash has 'Error' as a key, we raise an error
-  if result.has_key? 'Error'
-    raise "web service error"
-  end
+  raise "web service error" if result.has_key? 'Error'
 
-  return result
+  result
 end
 
+#
+# Convert the Hash response to a multiline output to be displayed on the screen
+# and the web.
+# 
+# @param [Hash] hash is the data in the format of a response from the WhitePages
+#   API.
+#
+def render_results(people_list)
+  
+  people = people_list['listings'].map do |people| 
+    
+    person_information = people['displayname'] + "\n" +
+      people['address']['fullstreet'] + "\n" +
+      people['address']['city'] + ", " + 
+      people['address']['state'] + " " +
+      people['address']['zip'] + "\n"
 
+    ### need to test to make sure that nothing is null:  not everyone has a telephone
+    if people['phonenumbers'] and people['phonenumbers'][0] and people['phonenumbers'][0]['fullphone']
+      person_information += people['phonenumbers'][0]['fullphone'] + "\n"
+    end
+      
+    person_information
+  end
+  
+  people.join("\n")
+  
+end
 
-def print_persons (hash)
-  people_list = hash
+def print_persons(people_list)
   puts "\e[H\e[2J"
-  print "\nList of results matching your query \n\n"
-  people_list['listings'].each do
-    |people| 
-    print people['displayname'], "\n"
-    print people['address']['fullstreet'], "\n" 
-    print people['address']['city'], ", "
-    print people['address']['state'], " "
-    print people['address']['zip'], "\n"
-
-### need to test to make sure that nothing is null:  not everyone has a telephone
-    if (!(people['phonenumbers'].nil? || people['phonenumbers'][0].nil? || people['phonenumbers'][0]['fullphone'].nil?) )
-      print people['phonenumbers'][0]['fullphone'], "\n"
-      end
-    print "\n"
-  end
-
+  puts "List of results matching your query\n\n"
+  puts render_results(people_list)
+  puts "\n\n"
 end
-
 
 ## print the output to the website
-def print_web (hash)
-  people_list = hash
-  display = []
+def print_web(people_list)
   get '/' do
-    people_list['listings'].each do
-      |people| 
-      display << people['displayname']
-      display << people['address']['fullstreet'] 
-      display << people['address']['city'] + ' ' + people['address']['state'] + ', ' + people['address']['zip']
-
-### need to test to make sure that nothing is null:  not everyone has a telephone
-      if (!(people['phonenumbers'].nil? || people['phonenumbers'][0].nil? || people['phonenumbers'][0]['fullphone'].nil?) )
-        display << people['phonenumbers'][0]['fullphone']
-      end
-      display << "\n"
-    end
-    "<thml><body><pre>#{display.join("\n")}</pre></body></htmml>"
+    "<html><body><pre>#{render_results(people_list)}</pre></body></htmml>"
   end
 end  
 
@@ -85,36 +78,33 @@ end
 
 
 #### function to query the user for the search parameters.  Need to ensure that the inputs don't have newlines or spaces between words
-def get_param ()
+def get_param
   puts "\e[H\e[2J"
   puts "Enter the first name:  "
   firstname = gets.chomp!                   #chomp to remove the newline from the input
   firstname.gsub!(/\s/,'+')                 # need to replace spaces with +
   puts "Enter the last name:  "
   lastname = gets.chomp!
-  lastname.gsub!(/\s/,'+')                  # need to replace spaces with +
 
-## need to ensure that the user enters a last name.  This is a requirement
-  while lastname == ""
+  ## need to ensure that the user enters a last name.  This is a requirement
+  while lastname.strip == ""
     puts "You can't have an empty lastname"
     puts "Enter the last name:  "
     lastname = gets.chomp!
-    lastname.gsub!(/\s/,'+')                 # need to replace spaces with +
-    end
+  end
 
+  lastname.gsub!(/\s/,'+')                  # need to replace spaces with +
+  
   puts "Enter the location (zip, city/state or state)"
   location = gets.chomp!
   location.gsub!(/\s/,'+')                  # need to replace spaces with +
 
-  params = [firstname, lastname, location]
+  [ firstname, lastname, location ]
 end
 
 
 
-search_param = get_param
-persons = person_search(search_param[0],search_param[1],search_param[2])
-print_persons (persons)
+firstname, lastname, location = get_param
+persons = person_search(firstname, lastname, location)
+print_persons(persons)
 print_web(persons)
-
-
-
